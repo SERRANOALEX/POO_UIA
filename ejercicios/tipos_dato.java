1657394843985669c505a4d75d53a27b917bff64
ALEJANDRO <alex.271960@hotmail.com>
2016-02-03 09:48:31 -0600

    Declara e inicializa tipos de Dato

---
+++ ejercicios/tipos_dato.java
@@ -0,0 +1,30 @@
+ class tipos_dato {
+
+ public static void main(String[] args) {
+ System.out.println("TIPOS DE DATO PRIMITIVOS ENTEROS\n");
+ byte A= 10;
+ short B = 200;
+ int C = 300;
+ long D = 400;
+ char letra = 'G'; 
+ System.out.println("\nTIPOS DE DATO BYTE VALE");
+      System.out.println( A  );
+      System.out.println("TIPOS DE DATO CHORT VALE");
+      System.out.println( B  );
+      System.out.println("TIPOS DE DATO INT VALE");
+      System.out.println(  C );      
+      System.out.println("TIPOS DE DATO LONG VALE");
+      System.out.println( D  );
+      System.out.println("TIPOS DE DATO CHAR VALE");
+      System.out.println( letra );  
+    
+     System.out.println("TIPOS DE DATO PRIMITIVOS REALES\n");
+     float E = 10;
+     double F = 20.6;
+     System.out.println("\nTIPOS DE DATO FLOAT VALE");
+     System.out.println( E );
+     System.out.println("TIPOS DE DATO DOUBLE VALE");
+     System.out.println( F );     
+    }
+    
+}
