a176508374ce0001d34f659b0224d28d7f99e76a
ALEJANDRO <alex.271960@hotmail.com>
2016-03-11 20:32:12 -0600

    EJERCICIO EXAMEN

---
+++ ejercicios/ejercicio_examen.java
@@ -0,0 +1,9 @@
+import java.oi*
+public class OrdenaCaracteres{
+	public Static void main(String args[]){
+	inputstreamReader = new cadenaInputstream("cadena");
+	System.out.println("PALABRA QUE SE DESEA ROTAR");
+	inputStreamreader=cadena.charAt();
+	System.out.println("PALABRA"+cadena.charAt);
+	}
+}
